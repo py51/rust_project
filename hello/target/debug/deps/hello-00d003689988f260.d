a2bff9c862282261d63ca9339fb5ac98697ee9d9
py51 <123048979+py51@users.noreply.github.com>
2023-02-08 14:39:23 +0000

    p1

---
+++ hello/target/debug/deps/hello-00d003689988f260.d
@@ -0,0 +1,8 @@
+/workspaces/rust_project1/hello/target/debug/deps/hello-00d003689988f260.rmeta: src/main.rs Cargo.toml
+
+/workspaces/rust_project1/hello/target/debug/deps/hello-00d003689988f260.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
---
+++ hello/target/debug/deps/hello-6199490722af14a4.d
@@ -0,0 +1,8 @@
+/workspaces/rust_project1/hello/target/debug/deps/hello-6199490722af14a4.rmeta: src/main.rs Cargo.toml
+
+/workspaces/rust_project1/hello/target/debug/deps/hello-6199490722af14a4.d: src/main.rs Cargo.toml
+
+src/main.rs:
+Cargo.toml:
+
+# env-dep:CLIPPY_ARGS=
